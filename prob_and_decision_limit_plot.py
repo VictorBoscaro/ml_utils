a4f9a1d234043153d457d73ba550af324d96ca44
VictorBoscaro <38840704+VictorBoscaro@users.noreply.github.com>
2023-09-04 15:46:09 -0300

    Create prob_and_decision_limit_plot.py

---
+++ prob_and_decision_limit_plot.py
@@ -0,0 +1,37 @@
+def plot_classifier(X, y, clf, ax=None, ticks=False, proba=False, lims=None): # assumes classifier "clf" is already fit
+    X0, X1 = X[:, 0], X[:, 1]
+    xx, yy = make_meshgrid(X0, X1, lims=lims)
+
+    if ax is None:
+        plt.figure()
+        ax = plt.gca()
+        show = True
+    else:
+        show = False
+
+    # can abstract some of this into a higher-level function for learners to call
+    cs = plot_contours(ax, clf, xx, yy, cmap=plt.cm.coolwarm, alpha=0.8, proba=proba)
+    if proba:
+        cbar = plt.colorbar(cs)
+        cbar.ax.set_ylabel('probability of red $\Delta$ class', fontsize=20, rotation=270, labelpad=30)
+        cbar.ax.tick_params(labelsize=14)
+    #ax.scatter(X0, X1, c=y, cmap=plt.cm.coolwarm, s=30, edgecolors='k', linewidth=1)
+    labels = np.unique(y)
+    if len(labels) == 2:
+        ax.scatter(X0[y==labels[0]], X1[y==labels[0]], cmap=plt.cm.coolwarm, s=60, c='b', marker='o', edgecolors='k')
+        ax.scatter(X0[y==labels[1]], X1[y==labels[1]], cmap=plt.cm.coolwarm, s=60, c='r', marker='^', edgecolors='k')
+    else:
+        ax.scatter(X0, X1, c=y, cmap=plt.cm.coolwarm, s=50, edgecolors='k', linewidth=1)
+
+    ax.set_xlim(xx.min(), xx.max())
+    ax.set_ylim(yy.min(), yy.max())
+#     ax.set_xlabel(data.feature_names[0])
+#     ax.set_ylabel(data.feature_names[1])
+    if ticks:
+        ax.set_xticks(())
+        ax.set_yticks(())
+#     ax.set_title(title)
+    if show:
+        plt.show()
+    else:
+        return ax
